60ba748cebe117402ee066e58879daf5037fb472
Mathieu Lutfy <mathieu@symbiotic.coop>
2020-03-20 16:57:20 -0400

    Disable buffering before downloading a file (fixes memory issues).

--- CRM/Aegirbackups/BAO/Aegirbackups.php
+++ CRM/Aegirbackups/BAO/Aegirbackups.php
@@ -54,6 +54,16 @@ class CRM_Aegirbackups_BAO_Aegirbackups {
         $contact_id = CRM_Core_Session::singleton()->get('userID');
         Civi::log()->info("Contact {$contact_id} has downloaded a full CiviCRM backup.");
 
+        // Turn off buffering to avoid memory issues, otherwise it's difficult to download
+        // a large file (large than the PHP memory limit).
+        // In Drupal7, the ob_get_level is initially at 3
+        // The 'test' variable is only to avoid infinite loops if something unexpected happens.
+        $test = 5;
+        while (ob_get_level() && $test > 0) {
+          ob_end_clean();
+          $test--;
+        }
+
         header('Content-Description: CiviCRM backup');
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename="' . basename($file) . '"');
